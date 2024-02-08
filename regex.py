2b757d85bbf28e70eedf69bef4a8f7168439e893
Robert Lutz <robertlutzi@gmail.com>
2024-02-08 09:04:52 -0500

    now returns the match, if found

--- regex.py
+++ regex.py
@@ -1,27 +1,38 @@
 def match(pattern, text, result=''):
+    """ String String -> Maybe String
+    a Reg Exp that finds matches where '*' is involved"""
     if pattern == '':
-        return True
+        return result
+    elif text == '':
+        return False
     yattern = pattern + '  '
     p, op, attern = yattern[0], yattern[1], yattern[2:]
     if op == '*':
-        if attern[0] == text[0] and (len(attern)==3 or match(attern[1:-2], text[1:])):
-            return True
-        elif p[0] == text[0] and match(pattern, text[1:]):
-            return True
-        else: 
-            return match(attern[:-2], text)
+        yikes = match(attern[1:-2], text[1:], result+text[0])
+        if attern[0] == text[0] and yikes:
+            return yikes
+        else:
+            yikes = match(pattern, text[1:], result+text[0])
+            if p[0] == text[0] and yikes:
+                return yikes
+            else: 
+                return match(attern[:-2], text, result)
     else:
-        if pattern[0] == text[0] and (len(pattern)==1 or match(pattern[1:], text[1:])):
-            return True
+        yikes = match(pattern[1:], text[1:], result+text[0])
+        if pattern[0] == text[0] and yikes:
+            return yikes
         else: 
             return False
 
+
+
 def tests():
-    assert (match("s*ha", "ssssssshark!")) == True
-    assert (match("s*ssha", "ssssssshark!")) == True
+    assert (match("s*ha", "ssssssshark!")) == "sssssssha"
+    assert (match("s*ssha", "ssssssshark!")) == "sssssssha"
     assert (match("s*ha", "sssshhark!")) == False
-    assert (match("s*ha", "hark!")) == True
-    assert (match("s*", "shark!")) == True
+    assert (match("s*ha", "hark!")) == "ha"
+    assert (match("s*", "shark!")) == "s"
+    assert (match("ps*", "pshark!")) == "ps"
     print("Tests pass!")
 
 tests()
