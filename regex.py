32b6666b6b71fb00e2ee4b689d269fedc0b905fe
Robert Lutz <robertlutzi@gmail.com>
2024-02-12 06:59:12 -0500

    separate functions for '*' and '?'

--- regex.py
+++ regex.py
@@ -4,42 +4,54 @@
 
 
 def match(pattern, text, result=''):
-    """ String String -> Maybe String
-        a Reg Exp that returns the first, simplest portion of text that matches pattern"""
+    """ String String String -> Maybe String
+        a Reg Exp that returns the first, greedy match"""
     lp = len(pattern)
     if lp == 0:
         return result
     elif text == '':
         return False
-    elif lp > 1 and pattern[1] in ('*', '?', '+'):
-        if pattern[1] in ('*', '?'):
-            return match_star(pattern[0], pattern[1], pattern[2:lp], text, result)
-        elif pattern[1] == '+':
-            return match_plus(pattern[0], pattern[1], pattern[2:lp], text, result)
+    elif lp > 1 and pattern[1] == '*':
+            return match_star(pattern[0], pattern[2:lp], text, result)
+    elif lp > 1 and pattern[1] == '?':
+            return match_perhaps(pattern[0], pattern[2:lp], text, result)
+    elif lp > 1 and pattern[1] == '+':
+            return match_plus(pattern[0], pattern[2:lp], text, result)
     elif pattern[0] == text[0]:
         return match(pattern[1:], text[1:], result+text[0])
     else:
         return False
         
 
-def match_star(p, op, pattern, text, result):
-    """ String String -> Maybe String
+def match_star(p, pattern, text, result):
+    """ String String String String -> Maybe String
         a Reg Exp that finds matches where '*' is involved"""
-    yikes = match(pattern, text, result)
+    yikes = match(pattern, text, result) # if you can match without using p, do so
     if len(pattern) > 0 and yikes:
         return yikes
     elif p == text[0]:
-        if op == '*':
-            return match_star(p, op, pattern, text[1:], result+text[0])
-        else:
-            return match(pattern, text[1:], result+text[0])
+        return match_star(p, pattern, text[1:], result+text[0])
     else:
         return yikes
     
 
-def match_plus(p, op, pattern, text, result):
-    if op == '+' and p == text[0]:
-        return match_star(p, '*', pattern, text[1:], result+text[0])
+def match_perhaps(p, pattern, text, result):
+    """ String String String String -> Maybe String
+        a Reg Exp that finds matches where '?' is involved"""
+    yikes = match(pattern, text, result) # if you can match without using p, do so
+    if len(pattern) > 0 and yikes:
+        return yikes
+    elif p == text[0]:
+        return match(pattern, text[1:], result+text[0])
+    else:
+        return yikes
+
+
+def match_plus(p, pattern, text, result):
+    """ String String String String -> Maybe String
+        a Reg Exp that finds matches where '+' is involved"""
+    if p == text[0]:
+        return match_star(p, pattern, text[1:], result+text[0])
     else:
         False
 
